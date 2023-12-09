8bb04e3369d6a1208607973091816ca99b3cfba9
Jonas Traweek <jonastraweek@yahoo.com>
2023-12-09 14:40:16 -0600

    Created obfuscated dataset

--- obfuscator.py
+++ obfuscator.py
@@ -22,7 +22,7 @@ def replaceChars(string):
     return string
     
 listOfZeros= ['0','2/2-1','8-8','2*3-6','SIN(0)']
-def modifyEquality(string):# I need to add something earlier to check if there are spaces before or after the equality such that I am missing terms. 
+def modifyEquality(string):# TODO change to directly target numbers instead.
     sides=string.split("=")
   
         
@@ -38,7 +38,7 @@ def modifyEquality(string):# I need to add something earlier to check if there a
                 sides[j]=sides[j]+"+"+listOfZeros[index]
    
     #Randomly select value
-    return str( sides[0]+"=" +sides[1])
+    return str( sides[0]+"=" +sides[1]+" ")
 
 
     
@@ -49,7 +49,7 @@ def changeLogicOperations (string): #Randomly chooses between written and signs,
     #TODO include spaces in s
     for element in orValues: 
         if (element in string): #TODO needs to be changed to not always pick first index
-            print("FoundElement")
+            #print("FoundElement")
             p1, sep, p2 = string.partition(element)
             whichString=random.randint(0,len(orValues)-1)
             extraOr = orValues[whichString]+listOfZeros[random.randint(0,len(listOfZeros)-1)]
@@ -61,8 +61,8 @@ def changeLogicOperations (string): #Randomly chooses between written and signs,
             
     for element in andValues:
         if (element in string):
-            print("FoundElement")
-            p1, sep, p2 = str1.partition(element)
+            #print("FoundElement")
+            p1, sep, p2 = string.partition(element)
             whichString=random.randint(0,len(andValues)-1)
             extraAND = andValues[whichString]+"1"
             whichString=random.randint(0,len(andValues)-1)
@@ -71,19 +71,39 @@ def changeLogicOperations (string): #Randomly chooses between written and signs,
                 
     return string
     
+#Start Here
 def obfuscateCommand(string):
-    sections=string.split("\'")
+ 
+   
+    singleIndex=string.find('\'')
+    doubleIndex=string.find('\"')
+    
+    if (singleIndex<doubleIndex and singleIndex!=-1):
+        sections=string.split("\'")
+        delimiter="\'"
+ 
+    else:
+        delimiter="\""
+        if (doubleIndex!=-1):   
+            sections=string.split("\"")
+            
+        else:
+            sections=[]
+            sections.append(string)
+  
+    #print(sections)
     output = ""
     for i in range(len(sections)):
-        if(i%2==1):
+        if(True):
             sections[i]=changeLogicOperations(sections[i])
-            
-            sections[i] = sections[i].replace(" =","=")
-            sections[i]=sections[i].replace("= ","=")
-            
+            while (" =" in sections[i]):
+                sections[i] = sections[i].replace(" =","=")
+            while ("= " in sections[i]):
+                sections[i]=sections[i].replace("= ","=")
+           # print("section Is "+sections[i])
             loopTemp=""
             terms=sections[i].split(" ")
-            print(terms)
+            #print(terms)
             for term in terms:
                 removedHash = term
                 postHash=""
@@ -92,7 +112,7 @@ def obfuscateCommand(string):
                     postHash="#"+postHash
                     
                 if ("=" in removedHash):
-                    print("Term is "+str(removedHash))
+                    #print("Term is "+str(removedHash))
                     loopTemp+=" "+str(modifyEquality(removedHash))+postHash
                 else:
                     loopTemp+=removedHash +postHash+" "
@@ -100,7 +120,7 @@ def obfuscateCommand(string):
             loopTemp=str(sections[i])
             
         if(i<len(sections)-1):   
-            loopTemp+="\'"
+            loopTemp+=delimiter
         output+=loopTemp
     #Add meaningless extra spaces?
 
@@ -117,10 +137,18 @@ def obfuscateCommand(string):
     return output
     
     
-print(random.randint(0,1))
-outputCommand = obfuscateCommand(sys.argv[1])
+commandList = [
+"  \" select * from users where id  =  \'1\' or @ @1  =  1 union select 1,version  (    )   -- 1\'\"",
+"admin'  )   or   (  '1'  =  '1",
+"\" select * from users where id  =  1 or \"\"&  (  \"\" or 1  =  1 -- 1\"",
+"\"1\"\"  )   where 1144  =  1144 and 2006  =  2006\""
+]
 
-print(outputCommand)
-percentObfs= replaceChars(outputCommand)
-print(percentObfs)
-print(outputCommand)
+for cmd in commandList:
+    outputCommand = obfuscateCommand(cmd)
+
+    print(cmd)
+    print(outputCommand)
+    percentObfs= replaceChars(outputCommand)
+    print(percentObfs)
+    print("")
